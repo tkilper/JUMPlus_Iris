f2be99addc8a43ef8583201a940a475faefdf7dc
Tristan Kilper <tkilperedu@gmail.com>
2021-12-09 14:20:32 -0500

    Tinkering with model 1

--- analysis.py
+++ analysis.py
@@ -9,7 +9,8 @@ print(' ')
 print('1) Import required libraries')
 print('--------------------------------------------------')
 import pandas as pd
-import sklearn as skl
+from sklearn import linear_model, model_selection
+from sklearn.metrics import mean_squared_error, r2_score
 import seaborn as sns
 import matplotlib.pyplot as plt
 print('Python libraries pandas, sklearn, seaborn, and matplotlib imported to file!')
@@ -21,7 +22,7 @@ print('--------------------------------------------------')
 df = pd.read_csv('C:/Users/Tristan Kilper/Desktop/JUMP/JUMPlus/Project3Iris/JUMPlus_Iris/Iris.csv')
 print('csv read!')
 print(' ')
-
+"""
 # 3) Show first records
 print('3) Show first records')
 print('--------------------------------------------------')
@@ -63,31 +64,49 @@ sns.scatterplot(data=df,x='SepalLengthCm',y='PetalLengthCm',hue='Species').set(t
 plt.show()
 print('scat_slvspl1.png')
 print(' ')
+"""
 
 print('  MODELS')
 print(' ')
 print('1) MODEL 1')
 # a) Create an object named as y which is storing the dataframe of a dependent variable names as 'sepallengthcm'
-y = df['SepalLengthCm']
+y = df[['SepalLengthCm']]
+print(y)
 
 # b) Create an object named as x which is storing the dataframe of an independent variable names as 'sepalwidthcm'
-x = df['SepalWidthCm']
+x = df[['SepalWidthCm']]
+print(x)
 
 # c) Divide the variables into x_train,x_test,y_train,y_test variables using train_test_split method carrying parameters named as x,y and test size should be 30%
-x_train, x_test, y_train, y_test = skl.model_selection.train_test_split(x,y,test_size=0.3,random_state=42)
+x_train, x_test, y_train, y_test = model_selection.train_test_split(x,y,test_size=0.3,random_state=42)
 
 # d) Show first five records of all four variables / objects
 print(x_train.head(5),x_test.head(5),y_train.head(5),y_test.head(5))
 
 # e) Create an object named as lr and assign memory from linearregression() method.
-lr = skl.linear_model.linearregression()
+lr = linear_model.LinearRegression()
 
 # f) Fit both training set into fit method
-xestimator = xestimator.fit(x_train)
-yestimator = yestimator.fit(y_train)
+lr.fit(x_train,y_train)
 
 # g) Predict x_test from predict method and store the result into y_pred obect
+y_pred = lr.predict(x_test)
+
 # h) Show first five records from actual and predicted objects
+print(y_test[0:5],y_pred[0:5])
+
 # i) Try to find out mean_squared_error in prediction using method after passing parameter as y_test and y_pred ,mind the result
+mse = mean_squared_error(y_test,y_pred)
+print(f'The mean squared error is {mse}')
+# The coefficients
+print("Coefficients: \n", lr.coef_)
+# The coefficient of determination: 1 is perfect prediction
+print("Coefficient of determination: %.2f" % r2_score(y_test, y_pred))
+
+# graph of the result
+print(type(x_test))
+sns.scatterplot(x=x_test,y=y_test,color='blue').set(title='Linear Regression on SepalWidth vs SepalLength')
+sns.lineplot(x=x_test,y=y_pred,color='orange')
+plt.show()
 
 print('2) MODEL 2')
